2c3d5fce2959521934a2fbaaa4ba10b121a13291
Katie Young <katie@sandstone.la>
2023-09-24 23:50:11 -0700

    packs seed completed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,5 @@
-// seed.ts
 import prisma from '../lib/prisma';
-import cardsData from "../public/cah-all-compact.json"
-
+import cardsData from '../public/cah-all-compact.json';
 
 async function main() {
   await prisma.user.create({
@@ -12,23 +10,39 @@ async function main() {
     },
   });
 
-  for (let i = 0; i < cardsData.white.length; i++) {
-    console.log(cardsData.white[i])
+  for (const whiteCard of cardsData.white) {
     await prisma.white.create({
       data: {
-        text: `${cardsData.white[i]}`
+        text: whiteCard,
       },
     });
+    console.log(`Created white card: ${whiteCard}`);
   }
 
-  for (let i = 0; i < cardsData.black.length; i++) {
-    console.log(cardsData.black[i])
+  for (const blackCard of cardsData.black) {
     await prisma.black.create({
       data: {
-        text: `${cardsData.black[i].text}`,
-        pick: cardsData.black[i].pick
+        text: blackCard.text,
+        pick: blackCard.pick,
       },
     });
+    console.log(`Created black card: ${blackCard.text}`);
+  }
+
+  for (const packData of cardsData.packs) {
+    const pack = await prisma.pack.create({
+      data: {
+        name: packData.name,
+        user: undefined,
+        whites: {
+          connect: packData.white.map((id) => ({ id })),
+        },
+        blacks: {
+          connect: packData.black.map((id) => ({ id })),
+        },
+      },
+    });
+    console.log(`Created pack: ${pack.name}`);
   }
 
   // Close Prisma client
